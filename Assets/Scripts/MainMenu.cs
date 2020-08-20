5904a4b38f9f35b040144bf1cce9617491ed504d
Ben Keener <keener4christ@gmail.com>
2020-08-20 16:30:17 -0400

    Completed task 12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
     public Button playButton;
     public Button quitButton;
 
+    public Toggle colorblindMode;
+
+    public Material trapMat;
+    public Material goalMat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +28,17 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+
         SceneManager.LoadScene("maze");
     }
 
