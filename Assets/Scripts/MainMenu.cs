331584b52cf6211faa269437522ede1e3047e263
Ben Keener <keener4christ@gmail.com>
2020-08-20 16:06:57 -0400

    Completed task 11

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,11 +7,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
+    public Button quitButton;
 
     // Start is called before the first frame update
     void Start()
     {
         playButton.onClick.AddListener(PlayMaze);
+        quitButton.onClick.AddListener(QuitMaze);
     }
 
     // Update is called once per frame
@@ -22,5 +24,11 @@ public class MainMenu : MonoBehaviour
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
     }
 }
